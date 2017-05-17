7d7884d626bf6e05d98b4512b973ed550db7833c
snhr <shinahara@human.ait.kyushu-u.ac.jp>
2017-05-18 08:18:10 +0900

    Not display the result when the confidence = 0.0%

--- python/ShowResult.py
+++ python/ShowResult.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, numpy
+import sys, numpy, math
 
 categories = numpy.loadtxt(sys.argv[1], str, delimiter="\t")
 scores = numpy.load(sys.argv[2])
@@ -8,4 +8,5 @@ top_k = 3
 prediction = zip(scores[0].tolist(), categories)
 prediction.sort(cmp=lambda x, y: cmp(x[0], y[0]), reverse=True)
 for rank, (score, name) in enumerate(prediction[:top_k], start=1):
-    print('#%d | %s | %4.1f%%' % (rank, name, score * 100))
+    if score * 100 != 0.0:
+        print('#%d | %s | %4.1f%%' % (rank, name, score * 100))
