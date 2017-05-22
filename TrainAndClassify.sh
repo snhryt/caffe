5f3e5899bcb2848c1e4f001f48162eab1ad34323
snhr <shinahara@human.ait.kyushu-u.ac.jp>
2017-05-22 20:54:42 +0900

    Add TrainAndClassify.sh

---
+++ TrainAndClassify.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+HDD_DIR="/media/snhryt/Data/Research_Master"
+SITUATION="10fonts_20class"
+WORK_DIR="./MyWork/${SITUATION}"
+NETWORK="caffenet"
+ITER_NUM="50000"
+TEST_DIRNAME="NeuropolNovaLt-Regular"
+
+mkdir -pv $WORK_DIR
+
+if [ ! -d "${WORK_DIR}/train_lmdb" ]; then
+	./build/tools/convert_imageset \
+	    / \
+      "${WORK_DIR}/train.txt" \
+	    "${WORK_DIR}/train_lmdb" \
+      -gray
+	echo ""
+fi
+if [ ! -d "${WORK_DIR}/val_lmdb" ]; then
+	./build/tools/convert_imageset \
+	    / \
+      "${WORK_DIR}/validation.txt" \
+      "${WORK_DIR}/val_lmdb" \
+      -gray
+	echo ""
+fi
+if [ ! -e "${WORK_DIR}/mean.npy" ]; then
+  if [ ! -e "${WORK_DIR}/mean.binaryproto" ]; then
+	  ./build/tools/compute_image_mean \
+        "${WORK_DIR}/train_lmdb" \
+        "${WORK_DIR}/mean.binaryproto"
+  fi
+  python ./python/ConvertBinaryprotoToNpy.py \
+      "${WORK_DIR}/mean.binaryproto" \
+      "${WORK_DIR}/mean.npy"
+  #rm "${WORK_DIR}/mean.binaryproto"
+	echo ""
+fi
+if [ ! -e "${WORK_DIR}/${NETWORK}_iter_${ITER_NUM}.caffemodel" ]; then
+	./build/tools/caffe train \
+      -solver \
+      "${WORK_DIR}/${NETWORK}_solver.prototxt"
+	echo ""
+fi
+
+python ./python/MakeRecogResultsTable.py \
+    "${HDD_DIR}/Syn_AlphabetImages/font/${TEST_DIRNAME}" \
+    "${WORK_DIR}/${NETWORK}_deploy.prototxt" \
+    "${WORK_DIR}/${NETWORK}_iter_${ITER_NUM}.caffemodel" \
+    "${HDD_DIR}/Syn_AlphabetImages/selected/100fonts_Clustering/SelectedFonts_20class.txt" \
+    "${HDD_DIR}/RecogResults/Clustering/${SITUATION}/${TEST_DIRNAME}.csv" \
+    --mean_img_filepath="${WORK_DIR}/mean.npy" 
+
+echo "\n=== ALL DONE! =============================="
