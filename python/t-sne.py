198e1f05348d60c726f65c36cf7f5991e6b90cac
snhr <shinahara@human.ait.kyushu-u.ac.jp>
2017-06-08 10:58:52 +0900

    mnistサンプルのt-SNE表示関数の追加

--- python/t-sne.py
+++ python/t-sne.py
@@ -3,15 +3,70 @@
 import numpy as np
 import sys
 import os
-import argparse
-import csv
 from sklearn import manifold
 from sklearn import datasets
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import glob
 import caffe
 
-def main(argv):
+def mnist_tsne():
+  parent_dirpath = "examples/mnist/"
+  model_def_filepath = parent_dirpath + "lenet.prototxt"
+  model_weight_filepath = parent_dirpath + "lenet_iter_5000.caffemodel"
+  mean_array = np.load(parent_dirpath + "mnist_mean.npy")
+
+  net = caffe.Classifier(
+    model_def_filepath,
+    model_weight_filepath,
+    mean=mean_array,
+    raw_scale=255
+  )
+  net.blobs['data'].reshape(1, 1, 28, 28)
+  net.reshape()
+
+  print "\n[Loading images]"
+  counter = 1
+  layer = "conv2"
+  feature = net.blobs[layer].data
+  #print "feature.shape = ",
+  #print feature.shape
+  num_output, width, height = feature.shape[1], feature.shape[2], feature.shape[3]  
+  input_imgs, indices = [], []
+  label_filepath = "data/mnist/ConvertMnistBinaryDataToImages/TestLabels.txt"
+  f = open(label_filepath)
+  line = f.readline()
+  while line:
+    str_counter = str(counter).zfill(5)
+    img_filepath = "data/mnist/10k_images/" + str_counter + ".png"
+    input_imgs.append(caffe.io.load_image(img_filepath, color=False))
+    indices.append(int(line.split("¥n")[0]))
+    if counter == 1:
+      feature = feature.reshape(1, num_output * width * height)
+    else:
+      r_feature = net.blobs[layer].data
+      r_feature = r_feature.reshape(1, num_output * width * height)
+      feature = np.vstack((feature, r_feature))     
+    if counter % 100 == 0:
+      print(".. %d images are loaded" % counter)
+      if counter == 1000:
+        break
+    counter += 1 
+    line = f.readline()
+  f.close()
+
+  print "\n[Calculate t-SNE]"
+  model = manifold.TSNE(n_components=2, perplexity=20.0, learning_rate=1000.0, n_iter=2000, verbose=3)
+  tsne = model.fit_transform(feature)
+  
+  plt.figure(1, figsize=(8, 8))
+  plt.clf()
+  for i in range(1, len(indices)):
+    plt.plot([tsne[i - 1, 0]], tsne[i, 0], tsne[i - 1, 1], tsne[i, 1], color=cm.hsv(indices[i]/10.0), marker="o")
+  plt.show()
+
+
+def main():
   parent_dirpath = "MyWork/2fonts_2class/"
   model_def_filepath = parent_dirpath + "caffenet_deploy.prototxt"
   model_weight_filepath = parent_dirpath + "caffenet_iter_30000.caffemodel"
@@ -28,12 +83,9 @@ def main(argv):
 
   print "\n[Loading images]"
   counter = 0
-  layer = "conv5"
-  #num_output, width, height = 96, 23, 23
-  #num_output, width, height = 256, 11, 11
-  #num_output, width, height = 384, 5, 5
-  #num_output, width, height = 384, 5, 5
-  num_output, width, height = 256, 5, 5
+  layer = "conv4"
+  feature = net.blobs[layer].data
+  num_output, width, height = feature.shape[1], feature.shape[2], feature.shape[3]  
   input_imgs, indices = [], []
   input_txt_filepath = parent_dirpath + "validation.txt"
   f = open(input_txt_filepath)
@@ -45,8 +97,6 @@ def main(argv):
     input_imgs.append(caffe.io.load_image(input_img_filepath, color=False))
     indices.append(index)
     if counter == 0:
-      feature = net.blobs[layer].data
-      print feature.shape
       feature = feature.reshape(1, num_output * width * height)
     else:
       r_feature = net.blobs[layer].data
@@ -76,4 +126,4 @@ def main(argv):
 
 
 if __name__ == "__main__":
-  main(sys.argv)
+  main()
